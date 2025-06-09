f4259425bb352dc6ab49c98193bb82f7c99af683
Vitaliy Obukh <Psiholog211@gmail.com>
2025-06-09 15:26:38 +0300

    fix noteService fail

--- src/services/noteService.ts
+++ src/services/noteService.ts
@@ -1,50 +1,49 @@
-import axios from "axios";
-import type { Note } from "../types/note";
+import axios from 'axios';
+import type { Note } from '../types/note';
 
 interface NotesHttpResponse {
-    notes: Note[];
-    totalPages: number;
+  notes: Note[];
+  totalPages: number;
 }
 
 interface NewNote {
-    title: string;
-    content: string;
-    tag: 'Todo' | 'Work' | 'Personal' | 'Meeting' | 'Shopping';
+  title: string;
+  content: string;
+  tag: 'Todo' | 'Work' | 'Personal' | 'Meeting' | 'Shopping';
 }
 
 const URL = 'https://notehub-public.goit.study/api/notes';
 
-export const fetchNotes = async(query: string, page: number): Promise<NotesHttpResponse> => { 
-    const parameters = new URLSearchParams({
-        ...(query !=='' ? {search: query}:{}),
-        page: page.toString(),
-    })
-    const response = await axios.get<NotesHttpResponse>(
-        `${URL}?${parameters}`, {
-        headers: {
-            Authorization: import.meta.env.VITE_NOTEHUB_TOKEN,
-        },
-});
-    return response.data;
+export const fetchNotes = async (
+  query: string,
+  page: number
+): Promise<NotesHttpResponse> => {
+  const parameters = new URLSearchParams({
+    ...(query !== '' ? { search: query } : {}),
+    page: page.toString(),
+  });
+  const response = await axios.get<NotesHttpResponse>(`${URL}?${parameters}`, {
+    headers: {
+      Authorization: import.meta.env.VITE_TMDB_TOKEN,
+    },
+  });
+  return response.data;
 };
 
-export const createNote = async(newNote: NewNote): Promise<Note> => {
-    const response = await axios.post<Note>(
-        `${URL}`, 
-        newNote, {
-        headers: {
-            Authorization: import.meta.env.VITE_NOTEHUB_TOKEN,
-        },
-});
-    return response.data;
+export const createNote = async (newNote: NewNote): Promise<Note> => {
+  const response = await axios.post<Note>(`${URL}`, newNote, {
+    headers: {
+      Authorization: import.meta.env.VITE_TMDB_TOKEN,
+    },
+  });
+  return response.data;
 };
 
-export const deleteNote = async(id: number): Promise<Note> => {
-    const response = await axios.delete<Note>(
-        `${URL}/${id}`, {
-        headers: {
-            Authorization: import.meta.env.VITE_NOTEHUB_TOKEN,
-        },
-});
-    return response.data;
-}
+export const deleteNote = async (id: number): Promise<Note> => {
+  const response = await axios.delete<Note>(`${URL}/${id}`, {
+    headers: {
+      Authorization: import.meta.env.VITE_TMDB_TOKEN,
+    },
+  });
+  return response.data;
+};

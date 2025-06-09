6f6374dc03173e279f22991d3c5f314cdf898b7b
Vitaliy Obukh <Psiholog211@gmail.com>
2025-06-09 15:52:32 +0300

    fix env fail

--- src/components/NoteList/NoteList.tsx
+++ src/components/NoteList/NoteList.tsx
@@ -1,41 +1,46 @@
 import css from './NoteList.module.css';
 import type { Note } from '../../types/note';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
-import { deleteNote } from '../../services/noteService';
+import { deleteNote } from '../../services/noteServicew';
 
 interface NoteListProps {
-    notes: Note[];
+  notes: Note[];
 }
 
-export default function NoteList({notes}: NoteListProps) {
+export default function NoteList({ notes }: NoteListProps) {
   const queryClient = useQueryClient();
 
   const mutation = useMutation({
     mutationFn: (id: number) => deleteNote(id),
     onSuccess: () => {
-      queryClient.invalidateQueries({queryKey: ['notes']});
+      queryClient.invalidateQueries({ queryKey: ['notes'] });
     },
   });
-  
+
   const handleDelete = (id: number) => {
-    mutation.mutate(id)
+    mutation.mutate(id);
   };
 
-    return (
-        <ul className={css.list}>
-	{notes.map(({title, content, tag, id}) => (
-  <li className={css.listItem} key={id}>
-    <h2 className={css.title}>{title}</h2>
-    <p className={css.content}>{content}</p>
-    <div className={css.footer}>
-      <span className={css.tag}>{tag}</span>
-      <button 
-      onClick={() => {handleDelete(id)}} 
-      className={css.button}
-      disabled={mutation.isPending}>
-        Delete
-      </button>
-    </div>
-  </li>))}
-</ul>)
+  return (
+    <ul className={css.list}>
+      {notes.map(({ title, content, tag, id }) => (
+        <li className={css.listItem} key={id}>
+          <h2 className={css.title}>{title}</h2>
+          <p className={css.content}>{content}</p>
+          <div className={css.footer}>
+            <span className={css.tag}>{tag}</span>
+            <button
+              onClick={() => {
+                handleDelete(id);
+              }}
+              className={css.button}
+              disabled={mutation.isPending}
+            >
+              Delete
+            </button>
+          </div>
+        </li>
+      ))}
+    </ul>
+  );
 }
